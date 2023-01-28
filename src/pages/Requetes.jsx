fbd294afa53178a30dac9673d41568e7b60e9120
eugene <bosukuege@gmail.com>
2023-01-28 12:15:17 +0100

    style:requetes_page

--- src/pages/Requetes.jsx
+++ src/pages/Requetes.jsx
@@ -18,14 +18,12 @@ function Requetes() {
   const [description, setDescription] = useState('')
   const [isOpen, setIsOpen] = useState(false)
   const [cate, setCate] = useState(null)
-  const [loading, setLoading] = useState(false)
   const cloudName = 'disyacex9'
   let navigate = useNavigate()
 
   useEffect(() => {
     socket.current = io('http://localhost:5200')
     socket.current.emit('add-user', currentUser._id)
-    setLoading(false)
   }, [currentUser])
 
   useEffect(() => {
@@ -87,40 +85,38 @@ function Requetes() {
     setIsOpen(true)
   }
   return (
-    <>
-      {loading ? (
-        <ContainerL>
-          <img src="" alt="" />
-        </ContainerL>
-      ) : (
-        <Container>
-          <div className="navbar">
-            <div className="page__title" onClick={() => navigate('/HomePage')}>
-              {' '}
-              <img src={utyLogo} alt="" className="uty__logo" />{' '}
-            </div>
-            <div className="count__container">
-              <IoNotifications
-                className="notify__icon"
-                onClick={() => navigate('/Offer')}
-              />
-              <IoMenu className="menu__icon" />
-            </div>
+    <Container>
+      <div className="navbar">
+        <div className="page__title" onClick={() => navigate('/HomePage')}>
+          {' '}
+          <img src={utyLogo} alt="" className="uty__logo" />{' '}
+        </div>
+        <div className="count__container">
+          <IoNotifications
+            className="notify__icon"
+            onClick={() => navigate('/Offer')}
+          />
+          <IoMenu className="menu__icon" />
+        </div>
+      </div>
+      <div className="request__form">
+        <div className="upload__side">
+          <h3>Salut {currentUser.username} trouvons votre produit</h3>
+          <div className="product__image">
+            <img src={picUrl} alt="" className="picture" />
           </div>
-          <div className="request__form">
-            <h3>Salut {currentUser.username} trouvons votre produit</h3>
-            <div className="product__image">
-              <img src={picUrl} alt="" className="picture" />
-            </div>
-            <div className="image__upload">
-              <input
-                type="file"
-                onChange={(e) => setSelectedImg(e.target.files[0])}
-                className="file"
-              />
-              <label htmlFor="file">Ajouter une image</label>
-            </div>
-            <span>Que voulez-vous?</span>
+          <div className="image__upload">
+            <input
+              type="file"
+              onChange={(e) => setSelectedImg(e.target.files[0])}
+              className="file"
+            />
+            <label htmlFor="file">Ajouter une image</label>
+          </div>
+        </div>
+        <div className="form__side">
+          <div className="input__side">
+            <span className="input__label">Que voulez-vous?</span>
             <textarea
               cols="30"
               rows="10"
@@ -128,7 +124,9 @@ function Requetes() {
               onChange={(e) => setDescription(e.target.value)}
               value={description}
             ></textarea>
+          </div>
 
+          <div className="select__side">
             <span>Sélectionnez la catégorie du produit</span>
             <select
               name="category"
@@ -140,15 +138,13 @@ function Requetes() {
                 <option key={cat}>{cat}</option>
               ))}
             </select>
-
-            <button onClick={(e) => handleSubmit(e)}>
-              Soumettre la requete
-            </button>
           </div>
-          {isOpen && <Modal setIsOpen={setIsOpen} />}
-        </Container>
-      )}
-    </>
+
+          <button onClick={(e) => handleSubmit(e)}>Soumettre la requete</button>
+        </div>
+      </div>
+      {isOpen && <Modal setIsOpen={setIsOpen} />}
+    </Container>
   )
 }
 
@@ -157,7 +153,6 @@ const Container = styled.div`
   padding-right: 5vw;
   display: flex;
   flex-direction: column;
-
   .navbar {
     display: flex;
     align-items: center;
@@ -204,73 +199,110 @@ const Container = styled.div`
   .request__form {
     display: flex;
     flex-direction: column;
-    h3 {
-      text-align: center;
-      margin-top: 2.5vh;
-    }
-    .image__upload {
+    width: 100%;
+    .upload__side {
       align-self: center;
-      position: relative;
-      .file {
-        opacity: 0;
-        position: absolute;
-        align-self: center;
+      h3 {
+        text-align: center;
+        margin-top: 2.5vh;
       }
-      label {
-        background-color: blue;
-        border-radius: 1rem;
-        height: 6.5vh;
-        color: white;
-        font-weight: bold;
-        width: 50vw;
+      .image__upload {
+        align-self: center;
+        position: relative;
+        .file {
+          opacity: 0;
+          position: absolute;
+          align-self: center;
+        }
+        label {
+          background-color: orange;
+          border-radius: 0.5rem;
+          height: 7.5vh;
+          color: white;
+          font-weight: bold;
+          font-size: 125%;
+          width: 80vw;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          cursor: pointer;
+          transition: transform 0.2s ease-out;
+          align-self: center;
+        }
+      }
+      .product__image {
+        height: 20vh;
+        width: 80vw;
+        margin-bottom: 2vh;
+        background-color: silver;
         display: flex;
-        align-items: center;
         justify-content: center;
-        cursor: pointer;
-        transition: transform 0.2s ease-out;
         align-self: center;
-      }
-    }
-    .product__image {
-      height: 15vh;
-      width: 30vw;
-      margin-bottom: 2vh;
-      background-color: silver;
-      display: flex;
-      justify-content: center;
-      align-self: center;
-      .picture {
-        height: 15vh;
+        padding-top: 1.5vh;
+        .picture {
+          height: 17.5vh;
+        }
       }
     }
 
-    span {
+    .form__side {
+      background-color: white;
+      /* background-image: linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%); */
+      box-shadow: 0px 0px 5px silver;
+      width: 100%;
+      border-top-right-radius: 2rem;
+      border-top-left-radius: 2rem;
       margin-top: 2.5vh;
-      margin-bottom: 1vh;
-    }
-    .request__input {
-      width: 90vw;
-      height: 7.5vh;
-      border-color: black;
-    }
-    select {
-      border-color: black;
-      height: 7.5vh;
-    }
-    button {
-      margin-top: 5vh;
-      height: 8vh;
-      background-color: red;
-      color: white;
-      font-size: 150%;
-      font-weight: bold;
-      border: none;
-      border-radius: 1rem;
-      margin-bottom: 7vh;
+      margin-left: -5vw;
+      margin-right: -10vw;
+      padding-left: 2.5vw;
+      padding-right: 8vw;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .input__side {
+        margin-top: 3.5vh;
+        display: flex;
+        flex-direction: column;
+        .input__label {
+          margin-bottom: 1vh;
+        }
+        .request__input {
+          width: 80vw;
+          height: 7.5vh;
+          border-color: black;
+          margin-right: -5vw;
+        }
+      }
+      .select__side {
+        display: flex;
+        flex-direction: column;
+        margin-left: 3.5vw;
+        select {
+          border-color: black;
+          height: 7.5vh;
+          width: 82.5vw;
+        }
+        span {
+          margin-top: 2.5vh;
+          margin-bottom: 1vh;
+        }
+      }
+      button {
+        margin-top: 5vh;
+        height: 8vh;
+        background-color: #020664;
+        color: white;
+        font-size: 125%;
+        font-weight: bold;
+        border: none;
+        border-radius: 0.5rem;
+        margin-bottom: 7vh;
+        margin-left: 2.5vw;
+        width: 82.5vw;
+      }
     }
   }
 `
-
-const ContainerL = styled.div``
 
 export default Requetes
